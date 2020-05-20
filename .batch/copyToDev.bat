855fe074341f0648acd2c2011ce91b3789129187
kaufman-bryce <kaufman.bryce@gmail.com>
2020-05-20 01:12:53 -0600

    Documented regex

--- .batch/copyToDev.bat
+++ .batch/copyToDev.bat
@@ -1,2 +1,16 @@
 .batch\kOS-Generic.ffs_batch
-.batch\fnr.exe --cl --dir "D:\KSP\KSP_1.8.1_KOS\Ships\Script" --fileMask "*.ks" --excludeFileMask "*.dll, *.exe" --includeSubDirectories --useRegEx --find "/(""[^""]*"")|(\/\/ #.*)| *?\/\/.*|^( |\t)*|( |\t)*$|( |\t)*([/*\-+=^{}(),<>:]|\. ?)( |\t)*|(?<= |\t)( |\t)+/gm" --replace "$1$2$6"
+
+:: Regex explaination
+:::::::::::::::::::::::
+:: ($1 first block ensures string literals are not touched in any way.) |
+:: ($2 second block similarly preserves // #comment-directives.) |
+:: third block catches line comments. |
+:: fourth catches leading (whitespace). |
+:: fifth catches trailing (whitespace). |
+:: sixth block looks for (whitespace) in front of ($6[operators] or| a .) followed by (whitespace),
+::     but preserves the space after a . for multiple commands in one line |
+:: last group does a (?<=lookbehind) to find multiple consecutive (whitespace) and reduce to one character,
+::     to collapse alignments that aren't attached to operators
+:: Remember to replace with $1$2$6 or it will remove strings, comment-directives and operators
+:: Designed to minify kOS scripts. kOS does not permit escaped quotes, so no considerations are made to account for them. If needed, modify the first capture group.
+.batch\fnr.exe --cl --dir "D:\KSP\KSP_1.8.1_KOS\Ships\Script" --fileMask "*.ks" --excludeFileMask "*.dll, *.exe" --includeSubDirectories --useRegEx --find "/(""[^""]*"")|(\/\/ #.*)| *?\/\/.*|^( |\t)*|( |\t)*$|( |\t)*([/*\-+=^<>{}()\[\],:]|\. ?)( |\t)*|(?<= |\t)( |\t)+/gm" --replace "$1$2$6"
